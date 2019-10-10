5785383f37fd4c10f13cac20562e5179b4f5991d
Tshepiso Tshegosebe <tshepiso@collivery.co.za>
2019-10-11 01:03:17 +0200

    Add a service to return validated requests to update and store * i know the appropiate way is to use form validation but i was getting errors when injecting the form validation request

---
+++ app/Http/Services/UserService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Services;
+
+use Illuminate\Support\Facades\Validator;
+
+class UserService
+{
+    public function validateUserRequests($request)
+    {
+        return Validator::make($request->all(), [
+            'id_no' => 'required|int|unique:users', //validate id_no to be unique
+            'firstname' => 'required|string',
+            'lastname' => 'required|string',
+            'gender' => 'required|string|max:1'
+        ]);
+    }
+}
